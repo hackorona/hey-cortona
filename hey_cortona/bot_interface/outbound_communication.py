4aa1fba4b0c4e483b870f1befea41177ab9f8f26
Ori Keidar <=>
2020-03-31 23:34:34 +0300

    Fix

--- hey_cortona/bot_interface/outbound_communication.py
+++ hey_cortona/bot_interface/outbound_communication.py
@@ -1,20 +1,6 @@
-import json
-from dataclasses import dataclass
-
 from twilio.rest import Client
 
-@dataclass
-class User:
-    number: str
-    name: str = None
-
-    def get_number(self) -> str:
-        return f"whatsapp:{self.number}"
-
-    @classmethod
-    def from_raw(cls, raw_text: str):
-        raw_text = raw_text.replace("whatsapp:", "")
-        cls(raw_text)
+from bot_interface.user import User
 
 
 class OutboundSender:
@@ -25,9 +11,9 @@ class OutboundSender:
 
         self._client: Client = Client(account_sid, auth_token)
 
-    def send(self, sender: User, recipent: User, message: str):
+    def send(self, sender: User, recipient: User, message: str):
         self._client.messages.create(
             body=message,
             from_=sender.get_number(),
-            to=recipent.get_number()
+            to=recipient.get_number()
         )
