6260964c9d8f1aebb8802a074f5f70abb2c736e6
shaked zeltzer <shaked.zeltzer1@gmail.com>
2020-04-03 22:42:44 +0300

    Fix typo

--- hey_cortona/nlp/classifier.py
+++ hey_cortona/nlp/classifier.py
@@ -32,7 +32,7 @@ class Classifier:
 
         print(f'\nqid: {qid}, sim_prec: {similarity_percentage}, question: {question.question}\n')
         if similarity_percentage >= self.pass_percentage:
-            self.questions_database.add_question(question)
+            self.questions_database.add_question(question.question, qid)
         else:
             self.questions_database.add_questions(question)
 
